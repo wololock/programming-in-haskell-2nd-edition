807e6fc754e2414904f0b3a058f1b04720fed6ee
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-01-09 09:57:26 +0100

    ch07: bin2int implemented using foldr

--- ch07_01_sandbox.hs
+++ ch07_01_sandbox.hs
@@ -84,6 +84,5 @@ compose = foldr (.) id
 type Bit = Int
 
 bin2int :: [Bit] -> Int
-bin2int bits = sum [w * b | (w,b) <- zip weights bits]
-               where
-                 weights = iterate (*2) 1
+bin2int = foldr (\x y -> x + 2 * y) 0
+
