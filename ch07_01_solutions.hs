1986eddc293cdc6052c879f0d734005421a710f9
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-01-12 21:08:00 +0100

    ch07: ex2 solution added

--- ch07_01_solutions.hs
+++ ch07_01_solutions.hs
@@ -3,5 +3,26 @@ module Chapter_07 where
 -- Ex. 1
 -- [f x | x <- xs, p x]
 ex1 :: (a -> Bool) -> (a -> b) -> [a] -> [b]
-ex1 = map . filter
+ex1 p f = map f . filter p
 
+-- Ex. 2
+
+-- a.
+all' :: (a -> Bool) -> [a] -> Bool
+all' p = foldr (\x y -> y && p x) True
+
+-- b.
+any' :: (a -> Bool) -> [a] -> Bool
+any' p = foldr (\x y -> y || p x) False
+
+-- c.
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' p (x:xs) | p x = x : (takeWhile' p xs)
+                    | otherwise = []
+
+-- d.
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' _ [] = []
+dropWhile' p (x:xs) | p x = dropWhile' p xs
+                    | otherwise = x:xs
