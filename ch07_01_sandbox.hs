a1eb4c316615647e20092045e77b8702100f5b8f
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-01-12 20:36:27 +0100

    ch07: alternative winner function added

--- ch07_01_sandbox.hs
+++ ch07_01_sandbox.hs
@@ -133,3 +133,26 @@ result vs = sort [(count v vs, v) | v <- rmdups vs]
 
 winner :: Ord a => [a] -> a
 winner = snd . last . result
+
+-- Alternative vote
+
+ballots :: [[String]]
+ballots = [["Red", "Green"],
+        ["Blue"],
+        ["Green", "Red", "Blue"],
+        ["Blue", "Green", "Red"],
+        ["Green"]]
+
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (/= []) 
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/= x))
+
+rank :: Ord a => [[a]] -> [a]
+rank = map snd . result . map head
+
+winner' :: Ord a => [[a]] -> a
+winner' bs = case rank (rmempty bs) of
+    [c]     -> c
+    (c:cs)  -> winner' (elim c bs)
