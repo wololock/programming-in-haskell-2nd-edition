6661e3b0b1b743d57e09f4e01dd683652bd47bf3
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-05-25 12:54:35 +0200

    ch08: data declaration examples added

--- ch08_00_sandbox.hs
+++ ch08_00_sandbox.hs
@@ -18,3 +18,25 @@ type Assoc k v = [(k,v)]
 
 find :: Eq k => k -> Assoc k v -> v
 find k t = head [v | (k',v) <- t, k == k']
+
+
+-- 8.2 Data declarations
+
+data Move = North | South | East | West
+
+move :: Move -> Pos -> Pos
+move North (x,y) = (x,y+1)
+move South (x,y) = (x,y-1)
+move East (x,y) = (x+1,y)
+move West (x,y) = (x-1,y)
+
+moves :: [Move] -> Pos -> Pos
+moves [] p = p
+moves (m:ms) p = moves ms (move m p)
+
+rev :: Move -> Move
+rev North = South
+rev South = North
+rev West = East
+rev East = West
+
