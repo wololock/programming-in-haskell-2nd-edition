8daef9655d93be55c70de938b51b515ce94f9117
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-09-30 21:51:20 +0200

    ch08: recursive Nat type

--- ch08_00_sandbox.hs
+++ ch08_00_sandbox.hs
@@ -62,3 +62,15 @@ safehead xs = Just (head xs)
 -- 8.3 New type declarations
 
 newtype Nat = N Int 
+
+-- 8.4 Recursive types
+
+data Nat' = Zero | Succ Nat'
+
+nat2int :: Nat' -> Int
+nat2int Zero     = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat :: Int -> Nat'
+int2nat 0 = Zero
+int2nat n = Succ (int2nat (n - 1))
