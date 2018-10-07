305e5c3f4d2c1b082f5535b01ee94d0e2fd88dff
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-10-07 22:37:13 +0200

    ch08: Tree data structure added

--- ch08_00_sandbox.hs
+++ ch08_00_sandbox.hs
@@ -88,3 +88,13 @@ data List a = Nil | Cons a (List a)
 len :: List a -> Int
 len Nil         = 0
 len (Cons _ xs) = 1 + len xs
+
+-- Let's define Tree type structure
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+t :: Tree Int
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+
+occurs :: Eq a => a -> Tree a -> Bool
+occurs x (Leaf y)    = x == y
+occurs x (Node a y b) = x == y || occurs x a || occurs x b
