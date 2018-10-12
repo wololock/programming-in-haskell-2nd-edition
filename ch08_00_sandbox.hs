514aaf755761739e96288810cda4f2b467e15409
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-10-12 21:40:56 +0200

    ch08: abstract machine example added

--- ch08_00_sandbox.hs
+++ ch08_00_sandbox.hs
@@ -169,4 +169,27 @@ substs p = map (zip vs) (bools (length vs))
 isTaut :: Prop -> Bool
 isTaut p = and [eval s p | s <- substs p]
 
+-- 8.7 Abstract machine
+
+data Expr = Val Int | Add Expr Expr
+
+value :: Expr -> Int
+value (Val n)   = n
+value (Add x y) = value x + value y
+
+type Cont = [Op]
+
+data Op = EVAL Expr | ADD Int
+
+eval' :: Expr -> Cont -> Int
+eval' (Val n) c   = exec c n
+eval' (Add x y) c = eval' x (EVAL y : c)
+
+exec :: Cont -> Int -> Int 
+exec [] n           = n
+exec (EVAL y : c) n = eval' y (ADD n : c)
+exec (ADD n : c) m  = exec c (n + m)
+
+value' :: Expr -> Int
+value' e = eval' e []
  
