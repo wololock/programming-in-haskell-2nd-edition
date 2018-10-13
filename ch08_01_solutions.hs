d5a6861da47579fe41fea450da782266c971746f
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-10-13 23:17:55 +0200

    ch08: ex 2 solution added

--- ch08_01_solutions.hs
+++ ch08_01_solutions.hs
@@ -19,3 +19,15 @@ mult (Succ m) n    = add (mult m n) n
 nat1 = Succ (Succ (Succ (Succ (Succ (Succ Zero)))))
 nat2 = Succ (Succ (Succ Zero))
 
+-- Ex. 2
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+t :: Tree Int
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+
+occurs :: Ord a => a -> Tree a -> Bool
+occurs x (Leaf y)     = x == y
+occurs x (Node l y r) = case compare x y of
+                          LT -> occurs x l
+                          EQ -> True
+                          GT -> occurs x r
