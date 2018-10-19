379a2c57cec43fd25e2a5ab488fad45fb96a3494
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-10-19 11:13:24 +0200

    ch09: added values and eval functions

--- ch09_00_sandbox.hs
+++ ch09_00_sandbox.hs
@@ -25,5 +25,29 @@ apply Sub x y = x - y
 apply Mul x y = x * y
 apply Div x y = x `div` y
 
+data Expr = Val Int | App Op Expr Expr
 
+instance Show Expr where
+    show (Val n)     = show n
+    show (App o l r) = brak l ++ show o ++ brak r
+                        where 
+                         brak (Val n) = show n
+                         brak e       = "(" ++ show e ++ ")" 
+
+
+-- 1 + (2 * 3)
+expr1 = (App Add (Val 1) (App Mul (Val 2) (Val 3)))                         
+
+-- Returns a list of values in expression
+values :: Expr -> [Int]
+values (Val n)     = [n]
+values (App _ l r) = values l ++ values r
+
+-- Evaluates expression where singleton list means a correct result
+-- and empty list means failure
+eval :: Expr -> [Int]
+eval (Val n)     = [n | n > 0]
+eval (App o l r) = [apply o x y | x <- eval l,
+                                  y <- eval r,
+                                  valid o x y]
 
