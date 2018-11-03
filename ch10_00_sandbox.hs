5c5319493fddd961e3fa0a37f779141aba5587cf
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-11-03 09:37:05 +0100

    ch10: getLine implementation

---
+++ ch10_00_sandbox.hs
@@ -0,0 +1,9 @@
+module Chapter_10_Sandbox where
+
+getLine' :: IO String
+getLine' = do x <- getChar
+              if x == '\n' then
+                return []
+              else
+                do xs <- getLine'
+                   return (x:xs)
