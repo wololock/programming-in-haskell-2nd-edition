7ba239968100441766e83752414c12e29f5962ac
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-11-03 09:53:43 +0100

    ch10: basic hangman implementation

--- ch10_00_sandbox.hs
+++ ch10_00_sandbox.hs
@@ -1,5 +1,7 @@
 module Chapter_10_Sandbox where
 
+import System.IO
+
 getLine' :: IO String
 getLine' = do x <- getChar
               if x == '\n' then
@@ -23,3 +25,42 @@ strlen = do putStr' "Enter a string: "
             putStr' "The string has "
             putStr' (show (length xs))
             putStrLn' " characters."
+
+-- Hangman
+hangman :: IO ()
+hangman = do putStrLn "Think of a word:"
+             word <- sgetLine
+             putStrLn "Try to guess it:"
+             play word
+
+sgetLine :: IO String
+sgetLine = do x <- getCh
+              if x == '\n' then
+                do putChar x
+                   return []
+              else
+                do putChar '-'
+                   xs <- sgetLine
+                   return (x:xs)
+
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           x <- getChar
+           hSetEcho stdin True
+           return x
+
+play :: String -> IO ()
+play word = do putStr "?"
+               guess <- getLine
+               if guess == word then
+                 putStrLn "You got it!"
+               else
+                 do putStrLn (match word guess)
+                    play word
+
+match :: String -> String -> String
+match xs ys = [if elem x ys then x else '-' | x <- xs]
+
+
+
+
