7721e75e1e48f0e11bd3f50a5916a1ea7a89702b
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-11-04 20:16:15 +0100

    ch10: game of life added

--- ch10_00_sandbox.hs
+++ ch10_00_sandbox.hs
@@ -135,3 +135,75 @@ nim board player = do newline
                                 nim board player
 
 
+-- Game Of Life
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int,Int)
+
+writeAt :: Pos -> String -> IO ()
+writeAt p xs = do goto p
+                  putStr xs
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+width :: Int
+width = 30
+
+height :: Int
+height = 30
+
+type Board' = [Pos]
+
+glider :: Board'
+glider = [(4,2), (2,3), (4,3), (3,4), (4,4)]
+
+showCells :: Board' -> IO ()
+showCells b = sequence_ [writeAt p "█" | p <- b]
+
+isAlive :: Board' -> Pos -> Bool
+isAlive b p = elem p b
+
+isEmpty :: Board' -> Pos -> Bool
+isEmpty b p = not (isAlive b p)
+
+neighbs :: Pos -> [Pos]
+neighbs (x,y) = map wrap [(x-1, y-1), (x, y-1),
+                          (x+1, y-1), (x-1, y),
+                          (x+1, y), (x-1, y+1),
+                          (x, y+1), (x+1, y+1)]
+
+wrap :: Pos -> Pos
+wrap (x,y) = (((x-1) `mod` width) + 1, ((y-1) `mod` height) + 1)
+
+liveneighbs :: Board' -> Pos -> Int
+liveneighbs b = length . filter (isAlive b) . neighbs
+
+survivors :: Board' -> [Pos]
+survivors b = [p | p <- b, elem (liveneighbs b p) [2,3]]
+
+births :: Board' -> [Pos]
+births b = [p | p <- rmdups (concat (map neighbs b)), 
+                isEmpty b p, 
+                liveneighbs b p == 3]
+
+rmdups :: Eq a => [a] -> [a]
+rmdups []     = []
+rmdups (x:xs) = x : rmdups (filter (/= x) xs)
+
+nextgen :: Board' -> Board'
+nextgen b = survivors b ++ births b
+
+wait :: Int -> IO ()
+wait n = sequence_ [return () | _ <- [1..n]]
+
+life :: Board' -> IO ()
+life b = do cls
+            showCells b
+            wait 2000
+            life (nextgen b)
+
+b1 :: Board'
+b1 = [(1,1), (5,4), (12,3), (12,4), (2,3), (5,5), (6,7), (5,4),(4,5), (12,4), (11,3), (11,4), (8,8), (7,8), (8,9), (7,9), (7,7), (6,7), (6,6), (5,6)]
