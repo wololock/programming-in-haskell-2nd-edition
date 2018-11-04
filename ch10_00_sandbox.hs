acef564e208169ae3ab356a3aa80cb779ec87121
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-11-04 17:14:39 +0100

    ch10: game of nim added

--- ch10_00_sandbox.hs
+++ ch10_00_sandbox.hs
@@ -2,6 +2,7 @@ module Chapter_10_Sandbox where
 
 import System.IO
 import Data.List
+import Data.Char
 
 getLine' :: IO String
 getLine' = do x <- getChar
@@ -65,5 +66,72 @@ match :: String -> String -> String
 match xs ys = [if elem x ys then x else '-' | x <- xs]
 
 
+-- Game of nim example
+
+next :: Int -> Int 
+next 1 = 2
+next 2 = 1
+
+type Board = [Int]
+
+initial :: Board
+initial = [5,4,3,2,1]
+
+finished :: Board -> Bool
+finished = all (== 0)
+
+valid :: Board -> Int -> Int -> Bool
+valid board row num = board !! (row - 1) >= num
+
+move :: Board -> Int -> Int -> Board
+move board row num = [update r n | (r,n) <- zip [1..] board]
+                     where
+                       update r n = if r == row then n - num else n
+
+putRow :: Int -> Int -> IO ()
+putRow row num = do putStr (show row)
+                    putStr ": "
+                    putStrLn (concat (replicate num "⏺"))
+
+putBoard :: Board -> IO ()
+putBoard [a,b,c,d,e] = do putRow 1 a
+                          putRow 2 b
+                          putRow 3 c
+                          putRow 4 d
+                          putRow 5 e
+
+getDigit :: String -> IO Int
+getDigit prompt = do putStr prompt
+                     x <- getChar
+                     newline
+                     if isDigit x then
+                       return (digitToInt x)
+                     else
+                       do putStrLn "ERROR: Invalid digit"
+                          getDigit prompt
+
+newline :: IO ()
+newline = putChar '\n'
+
+nim :: Board -> Int -> IO ()
+nim board player = do newline
+                      putBoard board
+                      if finished board then
+                        do newline
+                           putStr "Player "
+                           putStr (show (next player))
+                           putStrLn " wins!"
+                      else
+                        do newline
+                           putStr "Player "
+                           putStrLn (show player)
+                           row <- getDigit "Enter a row number: "
+                           num <- getDigit "Stars to remove: "
+                           if valid board row num then 
+                             nim (move board row num) (next player)
+                           else 
+                             do newline
+                                putStrLn "ERROR: Invalid move"
+                                nim board player
 
 
