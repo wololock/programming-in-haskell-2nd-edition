ec2a22def64a8732538e7264036eb6f8aab58d5d
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-11-16 07:22:58 +0100

    ch12: the state monad added

--- ch12_00_sandbox.hs
+++ ch12_00_sandbox.hs
@@ -97,3 +97,28 @@ pairs xs ys = do x <- xs
                  y <- ys
                  return (x,y)
 
+-- The state monad
+
+type State = Int
+
+newtype ST a = S (State -> (a, State))
+
+app :: ST a -> State -> (a, State)
+app (S st) x = st x
+
+instance Functor ST where
+    -- fmap :: (a -> b) -> ST a -> ST b
+    fmap g st = S (\s -> let (x, s') = app st s in (g x, s'))
+
+instance Applicative ST where
+    -- pure :: a -> ST a
+    pure x = S (\s -> (x,s))
+
+    -- (<*>) :: ST (a -> b) -> ST a -> ST b
+    stf <*> stx = S (\s -> let (f, s')  = app stf s
+                               (x, s'') = app stx s' in (f x, s''))
+
+instance Monad ST where
+    -- (>>=) :: ST a -> (a -> ST b) -> ST b
+    st >>= f = S (\s -> let (x, s') = app st s in app (f x) s')
+
