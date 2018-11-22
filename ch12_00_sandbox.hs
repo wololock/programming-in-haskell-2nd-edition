7733e5e957603e4a637e7eaf06a719ccb220ad3c
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-11-22 08:32:38 +0100

    ch12: ex 4 solution

--- ch12_00_sandbox.hs
+++ ch12_00_sandbox.hs
@@ -210,3 +210,27 @@ instance Functor Tree' where
 --    -- (<*>) :: (a -> b -> c) -> (a -> b) -> (a -> c)
 --    g <*> f = \x -> g x (f x)
 
+
+-- Ex. 4: There may be more than one way to make a parameterised type into an applicative
+--        functor. For example, the library Control.Applicative provides an alternative
+--        ‘zippy’ instance for lists, in which the function pure makes an infinite list of copies
+--        of its argument, and the operator <*> applies each argument function to the
+--        corresponding argument value at the same position. Complete the following
+--        declarations that implement this idea
+
+newtype ZipList a = Z [a] deriving Show
+
+instance Functor ZipList where
+    -- fmap :: (a -> b) -> ZipList a -> ZipList b
+    fmap g (Z xs) = Z (fmap g xs)
+
+instance Applicative ZipList where
+    -- pure :: a -> ZipList a
+    pure x = Z (repeat x) 
+    -- (<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
+    (Z gs) <*> (Z xs) = Z [g x | (g,x) <- zip gs xs]
+
+
+                           
+
+
