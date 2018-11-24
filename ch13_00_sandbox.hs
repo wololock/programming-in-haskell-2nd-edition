e4716be113c71be9d027a5c1b06473362eb682f4
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-11-24 23:18:59 +0100

    ch13: parser first commit

---
+++ ch13_00_sandbox.hs
@@ -0,0 +1,45 @@
+module Chapter_13_Sandbox where
+
+import Control.Applicative
+import Data.Char
+
+newtype Parser a = P (String -> [(a, String)])
+
+parse :: Parser a -> String -> [(a, String)]
+parse (P p) inp = p inp
+
+item :: Parser Char
+item = P (\inp -> case inp of
+                    []     -> []
+                    (x:xs) -> [(x, xs)])
+
+
+instance Functor Parser where
+    -- fmap :: (a -> b) -> Parser a -> Parser b
+    fmap g p = P (\inp -> case parse p inp of
+                            []         -> []
+                            [(v, out)] -> [(g v, out)])
+
+instance Applicative Parser where
+    -- pure :: a -> Parser a
+    pure v = P (\inp -> [(v, inp)])
+    -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+    pg <*> px = P (\inp -> case parse pg inp of
+                             []        -> []
+                             [(g,out)] -> parse (fmap g px) out)
+
+three :: Parser (Char, Char)
+three = do x <- item
+           item
+           z <- item
+           return (x,z)
+
+
+instance Monad Parser where
+    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    p >>= f = P (\inp -> case parse p inp of
+                           []        -> []
+                           [(v,out)] -> parse (f v) out)
+
+
+
