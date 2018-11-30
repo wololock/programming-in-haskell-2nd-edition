cc45b7573711c51c7f467dca54532cd4a394f625
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-11-30 23:27:43 +0100

    ch13: calculator added

--- ch13_00_sandbox.hs
+++ ch13_00_sandbox.hs
@@ -2,6 +2,8 @@ module Chapter_13_Sandbox where
 
 import Control.Applicative
 import Data.Char
+import Data.List
+import System.IO
 
 newtype Parser a = P (String -> [(a, String)])
 
@@ -160,5 +162,96 @@ eval xs = case (parse expr xs) of
             [(n, [])]  -> n
             [(_, out)] -> error ("Unused input " ++ out)
             []         -> error ("Invalid input")
+
+-- 13.9 Calculator
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int,Int)
+
+writeAt :: Pos -> String -> IO ()
+writeAt p xs = do goto p
+                  putStr xs
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           x <- getChar
+           hSetEcho stdin True
+           return x
+
+box :: [String]
+box = ["+---------------+",
+       "|               |",
+       "+---+---+---+---+",
+       "| q | c | d | = |",
+       "+---+---+---+---+",
+       "| 1 | 2 | 3 | + |",
+       "+---+---+---+---+",
+       "| 4 | 5 | 6 | - |",
+       "+---+---+---+---+",
+       "| 7 | 8 | 9 | * |",
+       "+---+---+---+---+",
+       "| 0 | ( | ) | / |",
+       "+---+---+---+---+"]
+
+buttons :: String
+buttons = standard ++ extra
+          where 
+            standard = "qcd=123+456-789*0()/"
+            extra = "QCD \ESC\BS\DEL\n"
+
+showbox :: IO ()
+showbox = sequence_ [writeAt (1,y) b | (y,b) <- zip [1..] box]
+
+display xs = do writeAt (3,2) (replicate 13 ' ')
+                writeAt (3,2) (reverse (take 13 (reverse xs)))
+
+calc :: String -> IO ()
+calc xs = do display xs
+             c <- getCh
+             if elem c buttons then
+                process c xs
+             else
+                do beep
+                   calc xs
+
+beep :: IO ()
+beep = putStr "\BEL"
+
+process :: Char -> String -> IO ()
+process c xs | elem c "qQ\ESC"    = quit
+             | elem c "dD\BS\DEL" = delete' xs
+             | elem c "=\n"       = eval' xs
+             | elem c "cC"        = clear
+             | otherwise          = press c xs
+
+quit :: IO ()
+quit = goto (1,14)
+
+delete' :: String -> IO ()
+delete' [] = calc []
+delete' xs = calc (init xs)
+
+eval' :: String -> IO ()
+eval' xs = case parse expr xs of
+             [(n,[])] -> calc (show n)
+             _        -> do beep
+                            calc xs
+
+clear :: IO ()
+clear = calc []
+
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+
+run :: IO ()
+run = do cls
+         showbox
+         clear
+
 
 
