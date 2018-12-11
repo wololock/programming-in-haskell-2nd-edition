fcfcd07f0fcf3205fd1d78cc80e7a9000f66795d
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-12-12 00:46:23 +0100

    ch14: foldables

--- ch14_00_sandbox.hs
+++ ch14_00_sandbox.hs
@@ -1,6 +1,7 @@
 module Chapter_14_Sandbox where
 
 import Data.Monoid
+import Data.Foldable
 
 -- instance Monoid a => Monoid (Maybe a) where
 --     -- mempty :: Maybe a
@@ -47,4 +48,38 @@ b1 = mconcat [All True, All True, All True]
 b2 :: Any
 b2 = mconcat [Any False, Any False, Any True]
 
+-- 14.2 Foldables
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+              deriving Show
+
+t1 :: Tree Char
+t1 = Node (Node (Leaf 'a') (Node (Leaf 'b') (Node (Leaf 'c') (Leaf 'd')))) (Node (Node (Leaf 'e') (Node (Leaf 'f') (Leaf 'g'))) (Leaf 'h'))
+
+instance Foldable Tree where
+    -- fold :: Monoid a => Tree a -> a
+    fold (Leaf x)   = x
+    fold (Node l r) = fold l <> fold r
+
+    -- foldMap :: Monoid b => (a -> b) -> Tree a -> b
+    foldMap f (Leaf x)   = f x
+    foldMap f (Node l r) = foldMap f l <> foldMap f r
+
+    -- foldr :: (a -> b -> b) -> b -> Tree a -> b
+    foldr f v (Leaf x)   = f x v
+    foldr f v (Node l r) = foldr f (foldr f v r) l
+
+    -- foldl :: (a -> b -> a) -> a -> Tree b -> a
+    foldl f v (Leaf x)   = f v x
+    foldl f v (Node l r) = foldl f (foldl f v l) r
+
+
+ex1 :: Int
+ex1 = getSum (foldMap Sum [1..10])
+
+ex2 :: Int
+ex2 = getProduct (foldMap Product [1..10])
+
+t2 :: Tree Int
+t2 = Node (Node (Leaf 1) (Node (Node (Leaf 3) (Leaf 4)) (Leaf 2))) (Node (Node (Leaf 6) (Node (Node (Leaf 8) (Leaf 9)) (Leaf 7))) (Leaf 5))
 
