7ae12c7b27718b5c938ac8b4e2b629e0b8ea1afd
Szymon Stepniak <szymon.stepniak@gmail.com>
2019-01-15 00:23:09 +0100

    ch15: exercise 6 added

--- ch15_00_sandbox.hs
+++ ch15_00_sandbox.hs
@@ -3,3 +3,21 @@
 
 fibs :: [Integer]
 fibs = 0 : 1 : [x + y | (x,y) <- zip fibs (tail fibs)]
+
+-- Ex. 6 Newton’s method for computing the square root of a (non-negative) floating-point
+-- number n can be expressed as follows:
+--
+--  * start with an initial approximation to the result;
+--  * given the current approximation a , the next approximation is defined by the
+--    function next a = (a + n/a) / 2 ;
+--  * repeat the second step until the two most recent approximations are within some
+--    desired distance of one another, at which point the most recent value is returned as
+--    the result.
+
+sqroot :: Double -> Double
+sqroot = snd . head . dropWhile (\(x,y) -> abs (x - y) >= dx) . seq
+    where
+        dx        = 0.00001
+        next n a  = (a + n/a) / 2
+        approxs n = iterate (next n) 1.0
+        seq n     = zip (approxs n) (drop 1 $ approxs n)
